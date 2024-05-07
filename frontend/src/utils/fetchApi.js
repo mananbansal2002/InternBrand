0af164ed4866a6d6e19479d17729828413feef1f
mananbansal2002 <71920970+mananbansal2002@users.noreply.github.com>
2024-05-07 18:36:00 +0530

    Update fetchApi.js

--- frontend/src/utils/fetchApi.js
+++ frontend/src/utils/fetchApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 axios.defaults.withCredentials=false;
-const url = 'http://localhost:8800/api'
+const url = 'https://internbrand-ekq2.onrender.com/'
 
 export const getDatas= async () => {
     return await axios.get(`${url}/getPosts`).catch((err)=>console.log(err));
